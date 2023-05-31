3dbfe31fdcb5a1746bc00331e42a763b30327022
MS07062000 <singhmurali.14@gmail.com>
2023-05-31 21:33:10 +0530

    removed some unnecessary files which I have not used

--- static/amadeus/internalapi.js
+++
@@ -1,8 +0,0 @@
-const baseURL=backendURL;
-async function getlocations(locationInput){
-    const locationURL="/locations";
-    let completeURL=baseURL+locationURL+encodeURIComponent(`/${locationInput}`);
-    let response=await fetch(completeURL);
-    let data=await response.json();
-    console.log(data);
-}
