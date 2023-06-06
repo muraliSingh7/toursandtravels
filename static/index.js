ae86badfbb01a9f951474eb31fa4eb8219f0d84f
MS07062000 <singhmurali.14@gmail.com>
2023-06-06 22:45:38 +0530

    Solved return date less than departdate Error

--- static/index.js
+++ static/index.js
@@ -26,21 +26,26 @@ addEventListener('DOMContentLoaded', (event) => {
         for (let i = 0; i < listOfTripInformationElement.length; i++) {
             let source = listOfTripInformationElement[i].shadowRoot.querySelector("input[name=From]").value;
             let destination = listOfTripInformationElement[i].shadowRoot.querySelector("input[name=To]").value;
-            let departdate = listOfTripInformationElement[i].shadowRoot.querySelector("input[name=DepartDate]").value;
+            let departdate = new Date(listOfTripInformationElement[i].shadowRoot.querySelector("input[name=DepartDate]").value);
             tripInformation["trip" + i] = {
                 'source': source.trim(),
                 'destination': destination.trim(),
-                'departdate': departdate.trim()
+                'departdate': departdate.toISOString().split('T')[0]
             };
 
 
             if (i == listOfTripInformationElement.length - 1) {
-                let returndate = listOfTripInformationElement[i].shadowRoot.querySelector("input[name=ReturnDate]").value;
+                let returndate =new Date(listOfTripInformationElement[i].shadowRoot.querySelector("input[name=ReturnDate]").value);
                 let adult = listOfTripInformationElement[i].shadowRoot.querySelector("input[name=Adult]").value;
                 let child = listOfTripInformationElement[i].shadowRoot.querySelector("input[name=Children]").value;
-                tripInformation['returndate'] = returndate.trim();
+                tripInformation['returndate'] = returndate.toString('yyyy-MM-dd');
                 tripInformation['adult'] = adult.trim();
                 tripInformation['child'] = child.trim();
+
+                if (departdate.getTime() >= returndate.getTime()) {
+                    displayErrorInViewArea("Return Date should be greater than or equal to Depart Date");
+                    return;
+                }
             }
 
             //checking all input fields are filled or not
@@ -66,41 +71,37 @@ addEventListener('DOMContentLoaded', (event) => {
 
             // console.log(JSON.stringify(tripInformation));
             if (inputFieldNotFilled) {
-                break;
+                displayErrorInViewArea("Please fill in all the required fields");
+                return;
             }
+
+
+            
         }
 
 
-
-        // console.log(inputFieldNotFilled);
-
-
-        if (inputFieldNotFilled) {
-            displayErrorInViewArea();
+        clearViewArea();
+        localStorage.clear();
+        if (tripType == "One-Way") {
+            let oneWaySearchResult = await oneWaySearch(tripInformation);
+            // console.log(oneWaySearchResult);
+            oneWaySearchResult = await processingData(oneWaySearchResult);
+            // console.log(finalResult);
+            localStorage.setItem("oneWaySearchResult", JSON.stringify(oneWaySearchResult));
+            let oneWayViewHandler = new OneWayAndRoundTripHandler(tripType, 0, tripInformation.trip0.source, tripInformation.trip0.destination, JSON.parse(localStorage.getItem("oneWaySearchResult"))[0]);
+            oneWayViewHandler.main();
+        } else if (tripType == "Round-Trip") {
+            let roundTripSearchResult = await processingData(await roundTripSearch(tripInformation));
+            // console.log(roundTripSearchResult);
+            localStorage.setItem("roundTripSearchResult", JSON.stringify(roundTripSearchResult));
+            let roundTripViewHandler = new OneWayAndRoundTripHandler(tripType, 0, tripInformation.trip0.source, tripInformation.trip0.destination, JSON.parse(localStorage.getItem("roundTripSearchResult"))[0]);
+            roundTripViewHandler.main();
         } else {
-            clearViewArea();
-            localStorage.clear();
-            if (tripType == "One-Way") {
-                let oneWaySearchResult = await oneWaySearch(tripInformation);
-                // console.log(oneWaySearchResult);
-                oneWaySearchResult = await processingData(oneWaySearchResult);
-                // console.log(finalResult);
-                localStorage.setItem("oneWaySearchResult", JSON.stringify(oneWaySearchResult));
-                let oneWayViewHandler = new OneWayAndRoundTripHandler(tripType, 0, tripInformation.trip0.source, tripInformation.trip0.destination, JSON.parse(localStorage.getItem("oneWaySearchResult"))[0]);
-                oneWayViewHandler.main();
-            } else if (tripType == "Round-Trip") {
-                let roundTripSearchResult = await processingData(await roundTripSearch(tripInformation));
-                // console.log(roundTripSearchResult);
-                localStorage.setItem("roundTripSearchResult", JSON.stringify(roundTripSearchResult));
-                let roundTripViewHandler = new OneWayAndRoundTripHandler(tripType, 0, tripInformation.trip0.source, tripInformation.trip0.destination, JSON.parse(localStorage.getItem("roundTripSearchResult"))[0]);
-                roundTripViewHandler.main();
-            } else {
-                let multiCitySearchResult = await multiCitySearch(tripInformation);
-                // console.log(multiCitySearchResult);
-                multiCitySearchResult = await processingData(multiCitySearchResult);
-                localStorage.setItem("multiCitySearchResult", JSON.stringify(multiCitySearchResult));
-                MultiTripHandler(tripType, tripInformation, JSON.parse(localStorage.getItem("multiCitySearchResult")));
-            }
+            let multiCitySearchResult = await multiCitySearch(tripInformation);
+            // console.log(multiCitySearchResult);
+            multiCitySearchResult = await processingData(multiCitySearchResult);
+            localStorage.setItem("multiCitySearchResult", JSON.stringify(multiCitySearchResult));
+            MultiTripHandler(tripType, tripInformation, JSON.parse(localStorage.getItem("multiCitySearchResult")));
         }
     });
 
@@ -108,15 +109,15 @@ addEventListener('DOMContentLoaded', (event) => {
 
 });
 
-function displayErrorInViewArea() {
+function displayErrorInViewArea(errorMessage) {
     let flightDataViewContainer = document.body.querySelector('[name=flightDataViewContainer]');
-    flightDataViewContainer.textContent = "Please fill in all the required fields";
+    flightDataViewContainer.textContent = errorMessage;
     flightDataViewContainer.setAttribute('class', 'displayError');
 }
 
 function clearViewArea() {
     let flightDataViewContainer = document.body.querySelector('[name=flightDataViewContainer]');
-    if(flightDataViewContainer.classList.contains('displayError')){
+    if (flightDataViewContainer.classList.contains('displayError')) {
         flightDataViewContainer.classList.remove('displayError');
     }
 
